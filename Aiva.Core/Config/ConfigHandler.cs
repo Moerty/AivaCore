e809cf7f2b30b708f16b4c1b61377461c9fa0069
Mo <mj@4c.de>
2019-01-18 12:41:05 +0100

    init

---
+++ Aiva.Core/Config/ConfigHandler.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Aiva.Core.Config {
+    public class ConfigHandler {
+        public static Models.Config.Model.Root Config { get; private set; }
+
+        public ConfigHandler() {
+            if(File.Exists(GetConfigPath())) {
+                Config = Models.Config.Model.Root.FromJson(
+                    File.ReadAllText(GetConfigPath()));
+            } else {
+                LoadDefaultConfigFile();
+
+                Config = Models.Config.Model.Root.FromJson(
+                    File.ReadAllText(GetConfigPath()));
+            }
+        }
+
+        /// <summary>
+        /// Move default config file to "real" file
+        /// </summary>
+        private void LoadDefaultConfigFile()
+            => File.Move(GetSampleConfigPath(), GetConfigPath());
+
+        /// <summary>
+        /// Save config to disc
+        /// </summary>
+        public void SaveConfig() {
+            var json = JsonConvert.SerializeObject(Config);
+            File.WriteAllText(GetConfigPath(), json);
+        }
+
+        /// <summary>
+        /// Get sample Config Path
+        /// </summary>
+        /// <returns></returns>
+        public static string GetSampleConfigPath() {
+            return Path.Combine(
+                AppContext.BaseDirectory,
+                "Config",
+                "config.json.default");
+        }
+
+        /// <summary>
+        /// Get Config Path
+        /// </summary>
+        /// <returns></returns>
+        public static string GetConfigPath() {
+            return Path.Combine(
+                AppContext.BaseDirectory,
+                "Config",
+                "config.json");
+        }
+    }
+}
---
+++ Aiva.Core/Twitch/AivaClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchLib.Api;
+using TwitchLib.Client;
+
+namespace Aiva.Core.Twitch {
+    public class AivaClient {
+        public bool DryRun = false;
+
+        public static TwitchClient TwitchClient { get; private set; }
+        public static TwitchAPI TwitchApi { get; private set; }
+
+        public AivaClient() {
+            TwitchApi = new TwitchAPI();
+            TwitchApi.Settings.ClientId = Config.ConfigHandler.Config.Credentials.TwitchClientID;
+            TwitchApi.Settings.AccessToken = Config.ConfigHandler.Config.Credentials.TwitchOAuth;
+        }
+    }
+}
