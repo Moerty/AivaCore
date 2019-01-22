d0a9ba887b52974df6306f32e2c04a1b3cbcb80b
Mo <mj@4c.de>
2019-01-22 19:49:54 +0100

    simple chat

--- Aiva.Gui/ViewModels/Tabs/ConsoleViewModel.cs
+++ Aiva.Gui/ViewModels/Tabs/ConsoleViewModel.cs
@@ -5,16 +5,17 @@ using System.Text;
 using Aiva.Gui.Internal;
 using TwitchLib.Client.Events;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Aiva.Gui.ViewModels.Tabs {
     [PropertyChanged.AddINotifyPropertyChangedInterface]
     public class ConsoleViewModel {
         public AsyncObservableCollection<Models.Gui.Tabs.Chat.MessageModel> Message { get; set; }
-        public AsyncObservableCollection<string> User { get; set; }
+        public AsyncObservableCollection<Models.Gui.Tabs.Chat.UserModel> User { get; set; }
 
         public ConsoleViewModel() {
             Message = new AsyncObservableCollection<Models.Gui.Tabs.Chat.MessageModel>();
-            User = new AsyncObservableCollection<string>();
+            User = new AsyncObservableCollection<Models.Gui.Tabs.Chat.UserModel>();
             AddListener();
         }
 
@@ -25,17 +26,67 @@ namespace Aiva.Gui.ViewModels.Tabs {
 
             Core.Twitch.AivaClient.TwitchClient.OnExistingUsersDetected
                 += (s, e)
-                => {
-                    e.Users.ForEach(u => User.Add(u));
-                };
+                => AddUsers(e);
 
             Core.Twitch.AivaClient.TwitchClient.OnUserJoined
                 += (s, e)
-                => User.Add(e.Username);
+                => UserJoined(e);
 
             Core.Twitch.AivaClient.TwitchClient.OnUserLeft
                 += (s, e)
-                => User.Remove(e.Username);
+                => RemoveUser(e);
+        }
+
+        private async void AddUsers(OnExistingUsersDetectedArgs e) {
+            foreach(var user in e.Users) {
+                var twitchUser = await Core.Twitch.AivaClient.TwitchApi.V5.Users.GetUserByNameAsync(user)
+                    .ConfigureAwait(false);
+
+                if (twitchUser?.Total > 0) {
+                    User.Add(
+                        new Models.Gui.Tabs.Chat.UserModel {
+                            UserId = twitchUser.Matches[0].Id,
+                            Username = twitchUser.Matches[0].DisplayName,
+                            UserType = "Viewer"
+                        });
+                }
+            }
+        }
+
+        private void RemoveUser(OnUserLeftArgs e) {
+            if(User.Any(u => u.Username == e.Username)) {
+                var user = User.FirstOrDefault(u => u.Username == e.Username);
+
+                User.Remove(user);
+            }
+        }
+
+        private async void UserJoined(OnUserJoinedArgs e) {
+            var twitchUser = await Core.Twitch.AivaClient.TwitchApi.V5.Users.GetUserByNameAsync(e.Username)
+                .ConfigureAwait(false);
+
+            if(twitchUser?.Total > 0) {
+                User.Add(
+                    new Models.Gui.Tabs.Chat.UserModel {
+                        UserId = twitchUser.Matches[0].Id,
+                        Username = twitchUser.Matches[0].DisplayName,
+                        UserType = "Viewer"
+                    });
+            }
+        }
+
+        private async Task UserJoined(string user) {
+            var twitchUser = await Core.Twitch.AivaClient.TwitchApi.V5.Users.GetUserByNameAsync(user)
+                .ConfigureAwait(false);
+
+            if (twitchUser?.Total > 0) {
+                User.Add(
+                    new Models.Gui.Tabs.Chat.UserModel {
+                        UserId = twitchUser.Matches[0].Id,
+                        Username = twitchUser.Matches[0].DisplayName,
+                        UserType = "Viewer"
+                    });
+            }
         }
 
         private void AddMessage(OnMessageReceivedArgs e) {
@@ -45,6 +96,25 @@ namespace Aiva.Gui.ViewModels.Tabs {
                     Message = e.ChatMessage.Message,
                     Badges = e.ChatMessage.Badges.Select(s => s.Key)
                 });
+
+            CheckUserRights(e);
+        }
+
+        private void CheckUserRights(OnMessageReceivedArgs e) {
+            if(User.Any(u => u.Username == e.ChatMessage.Username)) {
+                var user = User.FirstOrDefault(u => u.Username == e.ChatMessage.Username);
+
+                if(e.ChatMessage.UserType.ToString() != user.UserType) {
+                    user.UserType = e.ChatMessage.UserType.ToString();
+                }
+            } else {
+                User.Add(
+                    new Models.Gui.Tabs.Chat.UserModel {
+                        UserType = e.ChatMessage.UserType.ToString(),
+                        UserId = e.ChatMessage.UserId,
+                        Username = e.ChatMessage.Username
+                    });
+            }
         }
     }
 }
---
+++ Aiva.Models/Gui/Tabs/Chat/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aiva.Models.Gui.Tabs.Chat {
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class UserModel {
+        public string Username { get; set; }
+        public string UserType { get; set; }
+        public string UserId { get; set; }
+    }
+}
